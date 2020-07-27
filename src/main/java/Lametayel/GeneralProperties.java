08dfe23a9b6b963e6f3df80eaaa7930a795d91b4
SivanKimchi <61961052+SivanKimchi@users.noreply.github.com>
2020-07-27 09:25:56 +0300

    Update GeneralProperties.java

--- src/main/java/Lametayel/GeneralProperties.java
+++ src/main/java/Lametayel/GeneralProperties.java
@@ -3,14 +3,14 @@ package Lametayel;
 public class GeneralProperties {
 
     public static final String driverName = "webdriver.chrome.driver";
-    public static final String driverLocation = "C://Users//Sivan//Downloads//chromedriver.exe";
+    public static final String driverLocation = "{path to driver.exe}//chromedriver.exe";
 
     public static final String SiteURL1 = "https://www.lametayel.co.il/";
     public static final String SiteURLShop = "https://shop.lametayel.co.il/";
-    public static final String LoginEmail ="testseleniumwebdriver9@gmail.com";
-    public static final String LoginPassword ="sivantest123456";
+    public static final String LoginEmail ="{email}";
+    public static final String LoginPassword ="{password}";
     public static final String username ="earth_trekker";
-    public static final String userFirstName ="testSivan";
+    public static final String userFirstName ="testSivan"; 
     public static final String userLastName ="selenium";
     public static final String userMobilePhone ="0525555588";
     public static final String userCity ="תל אביב";
