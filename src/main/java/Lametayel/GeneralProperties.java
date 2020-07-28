3c27d59650880bca84335feccd159f6b41a0450c
Sivan <sivankimchi@gmail.com>
2020-07-28 10:51:24 +0300

    general

--- src/main/java/Lametayel/GeneralProperties.java
+++ src/main/java/Lametayel/GeneralProperties.java
@@ -3,7 +3,7 @@ package Lametayel;
 public class GeneralProperties {
 
     public static final String driverName = "webdriver.chrome.driver";
-    public static final String driverLocation = "{path to driver.exe}//chromedriver.exe";
+    public static final String driverLocation = "C://Users//Sivan//Downloads//chromedriver.exe";
 
     public static final String SiteURL1 = "https://www.lametayel.co.il/";
     public static final String SiteURLShop = "https://shop.lametayel.co.il/";
